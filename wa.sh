dc83272257e974160c89469985855c672141bf3c
Unkown00 <64455433+Unkown00BD@users.noreply.github.com>
2020-04-28 03:33:05 -0400

    Main Script

---
+++ wa.sh
@@ -0,0 +1,113 @@
+#!/bin/bash
+### Coded By Unkown00
+echo "GETTING READY TO SHOOT"
+green='\e[0;32m'
+white='\e[1;37m'
+red='\e[1;31m'
+function main_menu()
+{
+    while :
+    do
+echo -ne "\033]0;---Unkown00---\007"
+resize -s 24 109
+clear
+
+echo -e """
+------------------------------------------------------
+
+$red█░█░█ ▄▀█ ▄▄ ▀█▀ █▀█ █▀█ █░░ █▀    Ver | 1.0
+$green▀▄▀▄▀ █▀█ ░░ ░█░ █▄█ █▄█ █▄▄ ▄█    Dev | Unkown00
+------------------------------------------------------
+"""
+echo -e  """$white
+1] Google Scrapper         | 8] Revarse Ip Lookup
+2] Http Header             | 9] Banner Grabing
+3] Sql Vuln Web Scrapper   |10] UDP Port Scan
+4] Port Scanner            |11] Admin Panel Finder
+5] CMS Detector            |12] Hash Cracker
+6] Extract Page Links      | d] Download Hash List DB
+7] Subdomain Finder        |
+"""
+read -p "root@Unkown00:~ " option
+case "$option" in
+                d)cd modules
+                  wget https://www.dropbox.com/s/jwi4va9vfancx5q/worddb.zip
+                  unzip worddb.zip
+                  echo "DONE"
+                  ;;
+                1) cd modules
+                   read -p "Enter Dork To Scrape Links From Google:  " dork
+                   python3 gs.py $dork >> $dork.scrapped.txt
+                   echo "Result Saved in " | pwd "/ $dork.scrapped.txt"
+                   cd ..
+                   read -p "Enter Any Key To return"
+                   ;;
+               2) read -p "Enter Host without [http/https] with [www.] : " url
+                  curl -I $url
+                  read -p "Enter Any Key To return"
+                  ;;
+              3) echo "Getting Sql Vuln. Site From Google Using Dorks:"
+                cd modules
+                python3 gs.py inurl:news/news.php?id= >>sql.txt
+                python3 gs.py inurl:php?id=* >>_=*.sql1.txt
+                echo "Saved As sql.txt *.sql1.txt"
+                cd
+                ;;
+                4)read -p "Enter Ip Or Host To Scan :" ip
+                nmap $ip
+                read -p "press any key to return"
+;;
+            5)cd modules
+              read -p "Enter Host Name To Detect CMS :>" site
+             python cms-detector.py --site $site
+             read -p "press any key to return"
+             cd ..
+             ;;
+            6)echo "EXTRACT PAGE-LINKS"
+             read -p "Enter Website Full Url :> " url
+             curl https://api.hackertarget.com/pagelinks/?q=$url
+             read -p "press any key to return"
+             ;;
+            7) echo "SUBDOMAIN SCANNER"
+              read -p "Enter Website Full Url:> " url
+             curl https://api.hackertarget.com/hostsearch/?q=$url
+             read -p "press any key to return"
+              ;;
+            8)echo "REVARSE IP LOOKUP"
+              read -p "Enter Ip Adress: " ip
+              curl https://api.hackertarget.com/reverseiplookup/?q=$ip
+              read -p "press any key to return"
+              ;;
+            9)echo "Banner Grabing"
+              read -p "Enter Ip adreess or CIDR: " ip
+             curl https://api.hackertarget.com/bannerlookup/?q=$ip
+             read -p "press any key to return"
+             ;;
+           10)echo "UDP PORT SCAN"
+             read -p "Enter ip adress or Host Name To Start Scan: " ipu
+             nmap -sU  $ipu
+             read -p "press any key to return"
+             ;;
+           11)cd modules
+             perl admin.pl
+             cd ..
+             read -p "press any key to return"
+             ;;
+           12) echo "-----Hash Cracker----"
+              read -p "Enter Has To Crack::> " $hash
+              curl https://www.nitrxgen.net/md5db/$hash
+              echo "Checking In Worddb"
+              cd modules
+              grep -w "$hash" knownhashes
+              grep -w "$hash" hashplain
+              grep -w "$hash" hashplain2
+              grep -w "$hash" hashplain3
+              grep -w "$hash" hashplain4
+              grep -w "$hash" hashplain4
+              ;;
+
+
+          esac
+    done
+}
+main_menu
